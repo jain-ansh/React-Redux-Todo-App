0d6cfc336a5563211713f2dbecd242f923436d5b
jain-ansh <ansh05.aj@gmail.com>
2023-11-24 22:43:23 +0530

    Comments

--- src/reducers/todoListReducer.js
+++ src/reducers/todoListReducer.js
@@ -1,20 +1,22 @@
-const intialState = [];
+const intialState = ["Learn Js", "Learn Node"];
 
 export default function (state = intialState, action) {
     switch (action.type) {
-        case 'ADD_TODO':
-            // Not mutating data
-            // return [...state, action.payload];
-
+        case 'ADD_TODO': {
             // Mutating data
             state.unshift(action.payload);
             return state;
+
+            // Not mutating data
+            // return [...state, action.payload];
+        }
         case 'DELETE_TODO': {
             // Mutating the data
             state.splice(action.payload, 1);
+            console.log("State after deletion with Mutation", state);
             return state;
 
-            //Not mutating the data
+            // Not mutating the data
             // const newState = state.filter((list, index) => index !== action.payload);
             // return newState;
         }
